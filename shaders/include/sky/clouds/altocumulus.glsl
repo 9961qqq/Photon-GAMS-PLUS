232804db0a005625df87d6f40b61d2a316f9ac8f
9961QAQ <qqq114514eee@outlook.com>
2025-08-05 14:29:12 +0800

    调整天气

--- shaders/include/sky/clouds/altocumulus.glsl
+++ shaders/include/sky/clouds/altocumulus.glsl
@@ -193,7 +193,7 @@ CloudsResult draw_altocumulus_clouds(
 		daily_weather_variation.clouds_altocumulus_coverage.x
 	);
 
-	float extinction_coeff = mix(0.05, 0.1, day_factor) * CLOUDS_ALTOCUMULUS_DENSITY * (1.0 - 0.85 * high_coverage * (dampen(time_noon + time_midnight) * 0.75 + 0.25)) * (1.0 - 0.33 * rainStrength);
+	float extinction_coeff = mix(0.08, 0.16, day_factor) * CLOUDS_ALTOCUMULUS_DENSITY * (1.0 - 0.85 * high_coverage * (dampen(time_noon + time_midnight) * 0.75 + 0.25)) * (1.0 - 0.33 * rainStrength);
 	float scattering_coeff = extinction_coeff * mix(1.00, 0.66, rainStrength);
 
 	bool moonlit = sun_dir.y < -0.045;
